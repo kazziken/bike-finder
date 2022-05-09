e5bb0bcd33a47f02ddc240b14c8ca9e080ad34c6
Michelle <clairsin@gmail.com>
2022-05-09 15:44:01 -0400

    updated js with fetch

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+
+fetch('https://api.citybik.es/v2/networks')
+  .then (res => res.json())
+  .then (data => console.log(data))
