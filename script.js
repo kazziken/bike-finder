7088871845a15bd29aec34666f9cd9619bde277d
Michelle <clairsin@gmail.com>
2022-05-09 16:19:08 -0400

    updated footer

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
 
-
+//fetch request from citybikes api server
 fetch('https://api.citybik.es/v2/networks')
   .then (res => res.json())
   .then (data => console.log(data))
