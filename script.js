6da7e55187cb397c48eba679cc127a25ea47a3b3
Michelle <clairsin@gmail.com>
2022-05-09 17:13:29 -0400

    added header image

--- script.js
+++ script.js
@@ -2,7 +2,12 @@
 //fetch request from citybikes api server
 fetch('https://api.citybik.es/v2/networks')
   .then (res => res.json())
-  .then (data => console.log(data))
+  .then (data => console.log(data["networks"][0]))
 
 
-console.log('hello')
+// function rendersData (data) {
+//   for (let key in obj) {
+//     console.log(key)
+//   }
+// }
+
