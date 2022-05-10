dafaf553416515de70ba7baffa533284e3c4d7e4
Michelle <clairsin@gmail.com>
2022-05-10 11:59:04 -0400

    updated js

--- script.js
+++ script.js
@@ -1,13 +1,70 @@
 
+let cityUl = document.querySelector('#city-Ul')
+const searchSubmit = document.querySelector ('#form')
+
+
 //fetch request from citybikes api server
 fetch('https://api.citybik.es/v2/networks')
   .then (res => res.json())
   .then (data => {
     let networkData = data["networks"]
-    networkData.forEach (network => console.log(network))
+    networkData.forEach (network => renderCities(network))
     })
 
-console.log('hello')
+
+  function renderCities (network) {
+    cityName = document.createElement('li')
+    // cityName.sort()
+    cityUl.append(cityName)
+    cityName.innerHTML = network.location.city
+  }
+
+  searchSubmit.addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log('this is in search')
+    let child = cityUl.lastElementChild
+    while (child) {
+      cityUl.removeChild(child)
+      child = cityUl.lastElementChild
+    }
+    let networkID = document.querySelector('#form')[0].value
+    console.log(networkID)
+    
+    // fetch(`https://api.citybik.es/v2/networks/${networkID}`)
+    //   .then(res => res.json())
+    //   .then(data => matchName(data))
+
+    //   function matchName (network) {
+    //     console.log(network)
+        // let networkData = data["networks"]
+        // if("New York City" === networkData.location.city)
+        // console.log(network.company)
+      }
+  )
+
+
+
+
+  function matchName (network) {
+    let networkData = data["networks"]
+    if("New York City" === networkData.location.city)
+    console.log(network.company)
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // //@city
 //     fetch('https://api.citybik.es/v2/networks')
