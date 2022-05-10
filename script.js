78759e304006a06a916304b7eec9d83475a93f65
Michelle <clairsin@gmail.com>
2022-05-10 10:24:54 -0400

    updated html

--- script.js
+++ script.js
@@ -4,24 +4,27 @@ fetch('https://api.citybik.es/v2/networks')
   .then (res => res.json())
   .then (data => {
     let networkData = data["networks"]
-    console.log(networkData.location["city"])
     networkData.forEach (network => console.log(network))
     })
 
+console.log('hello')
 
+// //@city
+//     fetch('https://api.citybik.es/v2/networks')
+//   .then (res => res.json())
+//   .then (data => {
+//     let networkData = data["networks"]
+//     networkData.forEach(network => console.log(network.location.city))
+//     })
 
-//@city
-    fetch('https://api.citybik.es/v2/networks')
-  .then (res => res.json())
-  .then (data => {
-    let networkData = data["networks"]
-    networkData.forEach(network => console.log(network.location.city))
-    })
+// //@network
+//     fetch('https://api.citybik.es/v2/networks')
+//   .then (res => res.json())
+//   .then (data => {
+//     let networkData = data["networks"]
+//     networkData.forEach(network => console.log(network.location))
+//     })
 
-//@network
-    fetch('https://api.citybik.es/v2/networks')
-  .then (res => res.json())
-  .then (data => {
-    let networkData = data["networks"]
-    networkData.forEach(network => console.log(network.location))
-    })
+// // function getAllCities(city){
+// //   cities = document.
+// // }
